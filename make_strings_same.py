b728b0b665d6c98deb668194a1c44e20b6bdbee3
arundhati08mishra <50029223+arundhati08mishra@users.noreply.github.com>
2020-12-02 22:41:39 +0530

    Add files via upload

---
+++ make_strings_same.py
@@ -0,0 +1,28 @@
+'''
+Question: MAKE STRINGS SAME (from BinarySearch)
+Two strings s and t of same length n are given. You can pick one character in s and another in t and swap them. Given you can make unlimited number of swaps, return whether it's possible to make the two strings equal.
+
+Example :
+Input:	s = "ab"  t = "ba"
+
+Output: True
+
+
+Approach:
+We can only make the strings same when count of each character in both the strings combined is even.
+
+For eg:
+if we have 	s= 'abc' and  t= 'abd' converting one string to another is not possible as count of c and d is odd.
+
+But if we had s='abcd' and t='cabd' then we could make them same by swapping. 
+
+'''
+s = 'abbbbbbbbb'
+t = 'babbbbbbbb'
+
+str1=s+t 
+set1=set(str1) #to get unique characters , no repetition 
+for i in set1:
+	if str1.count(i)%2!=0:
+		return False
+return True
