ee55127570852e5560e9d8ab13adbec9b3fcb085
arundhati08mishra <50029223+arundhati08mishra@users.noreply.github.com>
2020-12-08 18:20:29 +0530

    Delete atbash_cipher.py

--- atbash_cipher.py
+++
@@ -1,6 +0,0 @@
-'''
-You are given a lowercase alphabet string text. Return a new string where every character in text is mapped to its reverse in the alphabet, so that a becomes z, b becomes y, c becomes x, and so on.
-
-'''
-srt1 = ord('a')+ord('z') 
-        return ''.join([chr(srt1 - ord(i)) for i in text]) 
